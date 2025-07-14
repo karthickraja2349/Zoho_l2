f0149fe53811acca4bf77986b8ce63d3f10657fb
karthickraja2349 <133229189+karthickraja2349@users.noreply.github.com>
2025-07-14 11:46:30 +0530

    Update level2.sh

--- level2.sh
+++ level2.sh
@@ -6097,7 +6097,7 @@ class Main {
 }
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 136)
-BinaryTree LevelOrder Traversal(LEETCODE 100)
+BinaryTree LevelOrder Traversal(LEETCODE 102)
 
 Given the root of a binary tree, return the level order traversal of its nodes values. (i.e., from left to right, level by level).
 Input: root = [3,9,20,null,null,15,7]
